8ca2bf090d4f6041d4dc7a14812f8f8baf91885e
TIKITA PERALTA <tikitaperalta@gmail.com>
2024-03-05 12:38:40 +0000

    find table

--- scrape.py
+++ scrape.py
@@ -2,8 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from datetime import date
+import csv
 
-URL = 'https://www.w3schools.com/charsets/ref_emoji_smileys.asp'
-req = requests.get(URL)
-soup = BeautifulSoup(req.content, "html.parser")
-results = soup.findAll("div", {"class": ""})
+URL = 'https://www.quackit.com/character_sets/emoji/emoji_v3.0/unicode_emoji_v3.0_characters_all.cfm'
+try:
+    req = requests.get(URL)
+    if req.status_code == 200:
+        # print(req.text)
+        print("Success!")
+    else:
+        print(f"Failed with status code: {req.status_code}")
+    soup = BeautifulSoup(req.content, "html.parser")
+    results = soup.findAll("div", {"class": "main"})
+    # print(results)
+    l = []
+    for res in results:
+        art = soup.find('article')
+        for a in art:
+            table = soup.find('table')
+    rows = table.find_all('tr')
+    list_of_rows = []
+    print(rows)
+    for row in rows:
+        cells = row.find_all('td')
+        list_of_rows.append(cells)
+
+        print(list_of_rows)
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+
+
+# for row in list_of_rows:
+#     print(row)
+
+# with open('data.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     field = ["Name", "Decimal"]
+    
+#     writer.writerow(field)
+#     for c in rows:
+#         writer.writerow([f'{name}'.lower, f'{decimal}'])
